788e7edef7c982cec6aace70396003bc62bdbbcf
Edmund Yau <164131599+edmunchies@users.noreply.github.com>
2024-03-20 12:46:43 -0700

    Update and rename ORDER BY.sql to ORDER BY & WHERE.sql

--- Udacity SQL for DA/ORDER BY & WHERE.sql
+++ Udacity SQL for DA/ORDER BY & WHERE.sql
@@ -43,3 +43,11 @@ SELECT *
 FROM orders 
 WHERE total_amt_usd < 500
 LIMIT 10;
+-- WHERE Clause
+The WHERE statement can also be used with non-numeric data. We can use the = and != operators here. You need to be sure to use single quotes (just be careful if you have quotes in the original text) with the text data, not double quotes.
+Commonly when we are using WHERE with non-numeric data fields, we use the LIKE, NOT, or IN operators. We will see those before the end of this lesson!
+
+Filter the accounts table to include the company name, website, and the primary point of contact (primary_poc) just for the Exxon Mobil company in the accounts table.
+  SELECT name, website, primary_poc
+  FROM accounts
+  WHERE name = 'Exxon Mobil';
