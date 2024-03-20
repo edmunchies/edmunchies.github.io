894890091f7cb7f1d1e9d1845f808c9bc9379c1d
Edmund Yau <164131599+edmunchies@users.noreply.github.com>
2024-03-20 12:01:48 -0700

    Update ORDER BY.sql

--- Udacity SQL for DA/ORDER BY.sql
+++ Udacity SQL for DA/ORDER BY.sql
@@ -20,6 +20,26 @@ FROM orders
 ORDER BY total_amt_usd 
 LIMIT 20;
 
-Write a query that displays the order ID, account ID, and total dollar amount for all the orders, sorted first by the account ID (in ascending order), and then by the total dollar amount (in descending order).
-Now write a query that again displays order ID, account ID, and total dollar amount for each order, but this time sorted first by total dollar amount (in descending order), and then by account ID (in ascending order).
-Compare the results of these two queries above. How are the results different when you switch the column you sort on first?
+  Write a query that displays the order ID, account ID, and total dollar amount for all the orders, sorted first by the account ID (in ascending order), and then by the total dollar amount (in descending order).
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY account_id, total_amt_usd DESC;
+  
+  Now write a query that again displays order ID, account ID, and total dollar amount for each order, but this time sorted first by total dollar amount (in descending order), and then by account ID (in ascending order).
+  Compare the results of these two queries above. How are the results different when you switch the column you sort on first?
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd DESC, account_id;
+
+Write a query that:
+  Pulls the first 5 rows and all columns from the orders table that have a dollar amount of gloss_amt_usd greater than or equal to 1000.
+SELECT * 
+FROM orders 
+WHERE gloss_amt_usd >= 100
+LIMIT 5;
+  
+  Pulls the first 10 rows and all columns from the orders table that have a total_amt_usd less than 500.
+SELECT * 
+FROM orders 
+WHERE total_amt_usd < 500
+LIMIT 10;
