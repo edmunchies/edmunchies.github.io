68c008db37429d8f36b97e826591b0b297f14b31
Edmund Yau <164131599+edmunchies@users.noreply.github.com>
2024-03-22 15:42:37 -0700

    Update AGGREGATION.sql

--- Udacity SQL for DA/AGGREGATION.sql
+++ Udacity SQL for DA/AGGREGATION.sql
@@ -44,3 +44,55 @@ GROUP BY can be used to aggregate data within subsets of the data. For example, 
 Any column in the SELECT statement that is not within an aggregator must be in the GROUP BY clause.
 The GROUP BY always goes between WHERE and ORDER BY.
 ORDER BY works like SORT in spreadsheet software.
+
+Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order.
+  SELECT a.name, o.occurred_at AS date
+  FROM accounts a
+  JOIN orders o 
+  ON a.id = o.account_id
+  ORDER BY date
+  LIMIT 1;
+
+Find the total sales in usd for each account. You should include two columns - the total sales for each company's orders in usd and the company name.
+  SELECT a.name, SUM(o.total_amt_usd) AS usd
+  FROM orders o 
+  JOIN accounts a
+  ON a.id = o.account_id 
+  GROUP BY a.name
+  ORDER BY a.name;
+  
+Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? Your query should return only three values - the date, channel, and account name.
+  SELECT w.occurred_at AS date, w.channel, a.name 
+  FROM web_events w
+  JOIN accounts a
+  ON w.account_id = a.id
+  ORDER BY date
+  LIMIT 1;
+  
+Find the total number of times each type of channel from the web_events was used. Your final table should have two columns - the channel and the number of times the channel was used.
+  SELECT w.channel, COUNT(channel) 
+  FROM web_events w
+  GROUP BY channel;
+  
+Who was the primary contact associated with the earliest web_event?
+  SELECT a.primary_poc 
+  FROM accounts a
+  JOIN web_events w
+  ON w.account_id = a.id
+  ORDER BY w.occurred_at
+  LIMIT 1;
+  
+What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.
+  SELECT a.name, o.total_amt_usd AS total_usd
+  FROM accounts a
+  JOIN orders o 
+  ON a.id = o.account_id
+  ORDER BY total_usd;
+  
+Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. Order from fewest reps to most reps.
+  SELECT r.name AS region, COUNT(s.name) AS sales_reps
+  FROM region r 
+  JOIN sales_reps s
+  ON s.region_id = r.id
+  GROUP BY region
+  ORDER BY sales_reps;
