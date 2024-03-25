01a783e2578ccf120520a9b600a04fcef392ae4a
Edmund Yau <164131599+edmunchies@users.noreply.github.com>
2024-03-25 16:09:12 -0700

    Update DATE & CASE.sql

--- Udacity SQL for DA/DATE & CASE.sql
+++ Udacity SQL for DA/DATE & CASE.sql
@@ -71,8 +71,7 @@ Write a query to display for each order, the account ID, total amount of the ord
   	CASE WHEN total_amt_usd >= 3000 THEN 'Large'
       ELSE 'Small'
       END AS order_size
-  FROM orders
-  ORDER BY total_amt_usd;
+  FROM orders;
 
 Write a query to display the number of orders in each of three categories, based on the total number of items in each order. The three categories are: 'At Least 2000', 'Between 1000 and 2000' and 'Less than 1000'.
 SELECT CASE WHEN o.total >= 2000 THEN 'At least 2000'
@@ -83,26 +82,26 @@ GROUP BY 1
 ORDER BY order_size;
 
 We would like to understand 3 different levels of customers based on the amount associated with their purchases. The top level includes anyone with a Lifetime Value (total sales of all orders) greater than 200,000 usd. The second level is between 200,000 and 100,000 usd. The lowest level is anyone under 100,000 usd. Provide a table that includes the level associated with each account. You should provide the account name, the total sales of all orders for the customer, and the level. Order with the top spending customers listed first.
-SELECT a.name, o.total_amt_usd AS total_sales, 
-	CASE WHEN o.total_amt_usd > 200000 THEN 'Level 1'
-    WHEN o.total_amt_usd >= 100000 AND o.total_amt_usd < 200000 THEN 'Level 2'
+SELECT a.name, SUM(o.total_amt_usd) AS total_sales, 
+	CASE WHEN SUM(o.total_amt_usd) > 200000 THEN 'Level 1'
+    WHEN SUM(o.total_amt_usd) >= 100000 AND SUM(o.total_amt_usd) < 200000 THEN 'Level 2'
     ELSE 'Level 3' END AS level
 FROM orders o 
 JOIN accounts a
 ON o.account_id = a.id
-GROUP BY a.name, o.total_amt_usd
+GROUP BY a.name
 ORDER BY level;
 
 We would now like to perform a similar calculation to the first, but we want to obtain the total amount spent by customers only in 2016 and 2017. Keep the same levels as in the previous question. Order with the top spending customers listed first.
-SELECT a.name, o.total_amt_usd AS total_sales,  
-	CASE WHEN o.total_amt_usd > 200000 THEN 'Level 1'
-    WHEN o.total_amt_usd >= 100000 AND o.total_amt_usd < 200000 THEN 'Level 2'
+SELECT a.name, SUM(o.total_amt_usd) AS total_sales,  
+	CASE WHEN SUM(o.total_amt_usd) > 200000 THEN 'Level 1'
+    WHEN SUM(o.total_amt_usd) >= 100000 AND SUM(o.total_amt_usd) < 200000 THEN 'Level 2'
     ELSE 'Level 3' END AS level
 FROM orders o 
 JOIN accounts a
 ON o.account_id = a.id
 WHERE o.occurred_at BETWEEN '2016-01-01' AND '2018-01-01'
-GROUP BY a.name, o.total_amt_usd
+GROUP BY a.name
 ORDER BY total_sales DESC;
 
 We would like to identify top performing sales reps, which are sales reps associated with more than 200 orders. Create a table with the sales rep name, the total number of orders, and a column with top or not depending on if they have more than 200 orders. Place the top sales people first in your final table.
